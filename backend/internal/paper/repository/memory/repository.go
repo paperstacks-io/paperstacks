19911e4110f22b61ef79bd6292ae15a3d661d828
Andreas Bauer <dev@andreasbauer.org>
2026-03-16 17:06:26 +0100

    Refactor Search func for paper

--- backend/internal/paper/repository/memory/repository.go
+++ backend/internal/paper/repository/memory/repository.go
@@ -89,36 +89,29 @@ func (r *Repository) Search(_ context.Context, title, keyword string) ([]domain.
 	defer r.mu.RUnlock()
 
 	var result []domain.Paper
-	set := make(map[string]struct{})
 
-	for _, item := range r.data {
-		titleMatch := false
-		keywordMatch := false
-
-		if title != "" && strings.Contains(strings.ToLower(item.Title), title) {
-			titleMatch = true
-		}
-
-		if keyword != "" {
-			for _, k := range item.Keywords {
-				if strings.Contains(strings.ToLower(k), keyword) {
-					keywordMatch = true
-					break
-				}
-			}
-		}
-
-		if titleMatch || keywordMatch {
-			if _, exist := set[item.Title]; !exist {
-				set[item.Title] = struct{}{}
-				result = append(result, item)
-			}
-		}
-	}
-
-	if len(result) == 0 {
+	if title == "" && keyword == "" {
 		return []domain.Paper{}, nil
 	}
 
+	for _, paper := range r.data {
+
+		matchesTitle := title != "" && strings.Contains(strings.ToLower(paper.Title), title)
+		matchesKeyword := keyword != "" && containsText(paper.Keywords, keyword)
+
+		if matchesTitle || matchesKeyword {
+			result = append(result, paper)
+		}
+	}
+
 	return result, nil
+}
+
+func containsText(slice []string, text string) bool {
+	for _, v := range slice {
+		if strings.Contains(strings.ToLower(v), text) {
+			return true
+		}
+	}
+	return false
 }
