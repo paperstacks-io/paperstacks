87d851342a4274d21c5a62bd2811a47f6f1f5d73
Will <w.steinleitner@icloud.com>
2026-05-07 22:59:17 +0200

    Simplify paper operations

--- backend/internal/stack/application/service.go
+++ backend/internal/stack/application/service.go
@@ -12,38 +12,35 @@ func NewStackService() *StackService {
 	return &StackService{}
 }
 
-// Add creates and stores a new stack in the in-memory repository.
+// Add creates and stores a new stack.
 // It returns an error if the stack could not be created
 // (e.g. the stack name already exists in the user's stack list).
-func (s *StackService) Add(user userDomain.User) error {
+func (s *StackService) Create(name string, user userDomain.User) error {
 	return nil
 }
 
-// AddPaper adds a paper to the specified stack.
-// It returns the updated stack and an error if the operation fails.
-//
-// An error is returned if:
-//   - the stack does not exist
-//   - the paper does not exist
-//   - the paper is already part of the stack
-//   - the stack reached the maximum number of papers
-func (s *StackService) AddPaper(user userDomain.User, stackUUID string, paperUUID string) (stackDomain.Stack, error) {
-	return stackDomain.Stack{}, nil
-}
-
-// RemovePaper removes a paper from the specified stack.
+// Update the specified stack.
 // It returns the updated stack and an error if the operation fails.
 //
 // An error is returned if:
 //   - the stack does not exist
 //   - the paper does not exist
 //   - the paper is not part of the stack
-func (s *StackService) RemovePaper(user userDomain.User, stackUUID string, paperUUID string) (stackDomain.Stack, error) {
+//   - the stack reached the maximum number of papers
+func (s *StackService) Update(user userDomain.User, stackUUID string, paperUUID string) (stackDomain.Stack, error) {
 	return stackDomain.Stack{}, nil
 }
 
-// Delete removes the specified stack from the in-memory repository.
+// Delete removes the specified stack from User.
 // It returns an error if the stack does not exist
 func (s *StackService) Delete(user userDomain.User, stackUUID string) error {
 	return nil
+}
+
+// List returns all stacks of the specified user.
+// This includes public and private stacks.
+//
+// It returns an error if the stacks could not be loaded.
+func (s *StackService) List(user userDomain.User) ([]stackDomain.Stack, error) {
+	return nil, nil
 }
