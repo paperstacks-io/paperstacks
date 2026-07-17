ef8159e549eab01150af761f98c512a645d0c81d
Andreas Bauer <dev@andreasbauer.org>
2026-07-17 14:34:52 +0200

    Add stack detail skeleton

--- backend/internal/web/routes.go
+++ backend/internal/web/routes.go
@@ -68,6 +68,8 @@ func AddRoute(
 	mux.Handle(http.MethodGet+" /stacks", defaultMiddle(handleStacksPage(logger, stacksTmpl, cfg.HankoAPIURL, stackService)))
 	stacksMyTmpl := pageTemplate("stacks/my")
 	mux.Handle(http.MethodGet+" /stacks/my", defaultMiddle(handleStacksMyPage(logger, stacksMyTmpl, cfg.HankoAPIURL, stackService)))
+	stacksDetailTmpl := pageTemplate("stacks/detail")
+	mux.Handle(http.MethodGet+" /stacks/detail", defaultMiddle(handlePage(stacksDetailTmpl, cfg.HankoAPIURL)))
 	mux.Handle(http.MethodPost+" /stacks/search", defaultMiddle(handleStacksSearchPublic(logger, tmpl, stackService)))
 	mux.Handle(http.MethodPost+" /stacks/my/search", defaultMiddle(handleStacksSearchByOwner(logger, tmpl, stackService)))
 	mux.Handle(http.MethodPost+" /stacks/my/stats", defaultMiddle(handleStacksStatsByOwner(logger, tmpl, stackService)))
